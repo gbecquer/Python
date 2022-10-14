77de6e367670efa57564634cb755afffe1feba2f
gbecquer <guille.becquer@gmail.com>
2022-10-14 20:34:03 +0200

    Day1
    Code for Day 1

---
+++ 100DaysOfCode/Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your childhood pet?\n")
+print("The name of your band could be: " + city + " " + pet)
